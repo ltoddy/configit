ef50bbcc8c2445533fd90323281bd3bd4ccbcbd0
ltoddy <taoliu0509@gmail.com>
2022-01-16 17:04:07 +0800

    chore: add Usage

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # configit
 
 Zero-boilerplate configuration management in Rust 
+
+## Installation
+
+```
+cargo add configit
+```
+
+## usage
+
+**config.toml**
+
+```toml
+host = "127.0.0.1"
+port = 8888
+```
+
+**src/main.rs**
+
+```rust
+use serde::Deserialize;
+use configit::Loader;
+
+#[derive(Debug, Deserialize)]
+pub struct AppConfig {
+    pub host: String,
+    pub port: u16,
+}
+
+fn main() {
+    let config = AppConfig::load_by("config.toml").expect("couldn't load `config.toml` file");
+    println!("config: {config:?}"); // config: AppConfig { host: "127.0.0.1", port: 8888 }
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,20 @@
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
+//! Zero-boilerplate configuration management
+//!
+//! ### Usage
+//!
+//! ```rust no_run
+//! use serde_derive::Deserialize;
+//! use configit::Loader;
+//!
+//! #[derive(Debug, Deserialize)]
+//! pub struct AppConfig {
+//!     host: String,
+//!     port: u16,
+//! }
+//!
+//! let config = AppConfig::load_by("config.toml").expect("couldn't load `config.toml` file");
+//! println!("config: {config:?}");
+//! ```
 
 #[cfg(not(any(feature = "toml_conf", feature = "yaml_conf")))]
 compile_error!(
